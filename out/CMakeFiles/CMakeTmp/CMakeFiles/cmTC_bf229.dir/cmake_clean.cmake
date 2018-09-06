de5782855b7d2508a3b2b884bf69c911bbcb53a9
Petru Petrica <petricap.gm@gmail.com>
2018-09-06 17:59:04 +0300

    Trying to render triangle

---
+++ out/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_bf229.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_bf229.dir/CheckFunctionExists.c.o"
+  "cmTC_bf229.pdb"
+  "cmTC_bf229"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_bf229.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 static int CompileShader(char shaderSource[], unsigned int type) {
     GLCall(unsigned int shaderID = glCreateShader(type));
     GLCall(glShaderSource(shaderID, 1, &shaderSource, NULL));
+
     GLCall(glCompileShader(shaderID));
+
     int compilationResult = 0;
     GLCall(glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compilationResult));
     if (compilationResult != GL_TRUE) {
@@ -19,6 +21,7 @@ static int CompileShader(char shaderSource[], unsigned int type) {
         fprintf(stderr, "Log: %s\n", log);
         GLCall(glDeleteShader(shaderID));
     }
+
     return shaderID;
 }
 
@@ -35,6 +38,7 @@ static unsigned int CreateGLSLProgram(unsigned int vertexShaderID,
         fprintf(stderr, "Failed to link program\n");
         return 0;
     }
+
     int infoLogLength;
     GLCall(glGetProgramiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength));
     if (infoLogLength > 0) {
@@ -47,11 +51,13 @@ static unsigned int CreateGLSLProgram(unsigned int vertexShaderID,
         fprintf(stderr, "Log: %s\n", log);
         free(log);
     }
+
     GLCall(glDetachShader(shaderID, vertexShaderID));
     GLCall(glDetachShader(shaderID, fragmentShaderID));
     GLCall(glDeleteShader(vertexShaderID));
     GLCall(glDeleteShader(fragmentShaderID));
     GLCall(glUseProgram(shaderID));
+
     return shaderID;
 }
 
@@ -59,20 +65,36 @@ int main() {
     renderel::graphics::Window window(960, 540, "Hello");
     window.setClearColor(0.7f, 0.2f, 0.2f);
 
-    float positions[] = {0.5f, 0.5f, 0.0f, -0.5f, -0.5f, 0.5f};
+    float positions[] = {
+        0.5f,  0.5f,  // 0
+        0.0f,  -0.5f, // 1
+        -0.5f, 0.5f   // 2
+    };
 
     unsigned int vao;
-    glGenBuffers(1, &vao);
-    glBindBuffer(GL_VERTEX_ARRAY, vao);
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
 
     unsigned int vbo;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &vbo));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions,
+                        GL_STATIC_DRAW));
+
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0));
+
+    unsigned int indices[] = {0, 1, 2};
+
+    unsigned int ibo;
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                        GL_STATIC_DRAW));
 
     char vertexShaderSource[] = "#version 330 core\n"
                                 "\n"
-                                "in vec2 position;\n"
+                                "layout(location = 0) in vec2 position;\n"
                                 "\n"
                                 "void main() {\n"
                                 "   gl_Position = vec4(position, 0.0f, 1.0f);\n"
@@ -80,7 +102,7 @@ int main() {
 
     char fragmentShaderSource[] = "#version 330 core\n"
                                   "\n"
-                                  "out vec4 color;"
+                                  "layout(location = 0) out vec4 color;\n"
                                   "\n"
                                   "void main() {"
                                   "   color = vec4(0.2f, 0.2f, 0.8f, 1.0f);\n"
@@ -96,7 +118,7 @@ int main() {
 
     while (!window.ShouldClose()) {
         window.PollEvents();
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        GLCall(glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0));
         window.Clear();
     }
 }
