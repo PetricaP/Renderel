5f162c619ba8ba15593fcded9953f33b11c4c12f
Petru Petrica <petricap.gm@gmail.com>
2018-09-16 14:26:21 +0300

    Improved debugger messages.

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -1,6 +1,8 @@
 #include "Debug.hpp"
 #include <GL/glew.h>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 void GLClearError() {
 	while (glGetError() != GL_NO_ERROR) {
@@ -9,9 +11,24 @@ void GLClearError() {
 }
 
 bool GLLogCall(const char *function, const char *file, int line) {
+
+	static const std::string GLErrors[] = {"GL_INVADIL_ENUM",
+										   "GL_INVALID_VALUE",
+										   "GL_INVALID_OPERATION",
+										   "GL_STACK_OVERFLOW",
+										   "GL_STACK_UNDERFLOW",
+										   "GL_OUT_OF_MEMORY",
+										   "GL_INVALID_FRAMEBUFFER_OPERATION",
+										   "GL_CONTEXT_LOST",
+										   "GL_TABLE_TOO_LARGE"};
+
 	GLenum error;
+	std::stringstream ss;
 	while ((error = glGetError())) {
-		fprintf(stderr, "[OpenGL Error] (%d): %s %s: %d\n", error, function,
+		ss << std::hex << error;
+		std::cout << error << '\n';
+		fprintf(stderr, "[OpenGL Error] (0x%s : %s): %s %s: %d\n",
+				ss.str().c_str(), GLErrors[error - 1280].c_str(), function,
 				file, line);
 		return false;
 	}
